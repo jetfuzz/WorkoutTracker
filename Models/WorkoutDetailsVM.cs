c97171cb530606805e6b2d21a519534709cac78f
jetfuzz <1jsf23@gmail.com>
2026-03-10 09:00:31 -0500

    Add PR tracking on workout details page

---
+++ Models/WorkoutDetailsVM.cs
@@ -0,0 +1,18 @@
+﻿namespace WorkoutTracker.Models
+{
+    public class WorkoutDetailsVM
+    {
+        public int WorkoutId { get; set; }
+        public string? Name { get; set; }
+        public DateTime Date { get; set; }
+        public List<WorkoutExerciseDetailsVM> Exercises { get; set; } = new();
+    }
+
+    public class WorkoutExerciseDetailsVM
+    {
+        public int ExerciseId { get; set; }
+        public string? ExerciseName { get; set; }
+        public List<SetVM> Sets { get; set; } = new();
+        public bool isBestWeight { get; set; }
+    }
+}
